bcef572ccee3917e88a5876b656e5dacce95bff0
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-05-15 13:56:44 -0300

    first commit

---
+++ locacaoVeiculos/src/main/java/com/weg/locacaoVeiculos/LocacaoVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.weg.locacaoVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocacaoVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocacaoVeiculosApplication.class, args);
+	}
+
+}
---
+++ locacaoVeiculos/src/test/java/com/weg/locacaoVeiculos/LocacaoVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.locacaoVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocacaoVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
